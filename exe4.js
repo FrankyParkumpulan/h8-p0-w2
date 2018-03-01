1bf2b4b8e684319d7da6ac8ca750911fe972e77e
FrankyParkumpulan <frankysianturi199@gmail.com>
2018-03-01 18:38:28 +0700

    exercise4

---
+++ exe4.js
@@ -0,0 +1,29 @@
+var tanggal =1 ;
+var bulan = 3;
+var tahun = 1;
+switch (bulan) {
+  case 1: { console.log('Januari'); 
+  break;}
+  case 2: { console.log('Februari'); 
+  break;}
+  case 3: { console.log('Januari'); 
+  break;}
+  case 4: { console.log('April');
+  break; }
+  case 5: { console.log('Mei'); 
+  break; }
+  case 6: { console.log('Juni');
+  break; }
+  case 7: { console.log('Januari');
+  break; }
+  case 8: { console.log('August'); 
+  break; }
+  case 9: { console.log('September');
+  break; }
+  case 10: { console.log('Oktober');
+  break; }
+  case 11: { console.log('November');
+  break; }
+  case 12: { console.log('December');
+  break; }
+}
