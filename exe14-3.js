17f8b37929dae2b4ed1c6df71c150fe84a26c506
FrankyParkumpulan <frankysianturi199@gmail.com>
2018-03-09 23:11:14 +0700

    exe14-3.js

--- exe14-3.js
+++ exe14-3.js
@@ -1,17 +1,17 @@
 function mengelompokkanAngka(arr) {
-  var newArray = [];
+  var new = [];
   
   for(var i=0; i<3; i++){
-    newArray.push([]);
+    new.push([]);
   }
   
   for(var j = 0; j < arr.length; j++){
     if(arr[j] % 3 === 0){
-      newArray[2].push(arr[j]);
+      new[2].push(arr[j]);
     } else if(arr[j] % 2 !== 0){
-      newArray[1].push(arr[j]);
+      new[1].push(arr[j]);
     } else if(arr[j] % 2 === 0){
-      newArray[0].push(arr[j]);
+      new[0].push(arr[j]);
     }
     // console.log(arr[j]);
   }
